68b20a61bbb5d91a6d1ed6c7323fe674f1d03255
henry199101 <331243543@qq.com>
2018-02-15 11:01:22 +0800

    modify index page, 增加表单验证功能。

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,12 +1,20 @@
-from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+	name = StringField('What is your name?', validators=[DataRequired()])
+	submit = SubmitField('Submit')
 
 
 @app.errorhandler(404)
@@ -19,15 +27,14 @@ def internal_server_error(e):
 	return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html',
-			       current_time=datetime.utcnow())
-
-
-@app.route('/user/<name>')
-def user(name):
-	return render_template('user.html', name=name)
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.hmtl', form=form, name=name)
 
 
 if __name__ == '__main__':
