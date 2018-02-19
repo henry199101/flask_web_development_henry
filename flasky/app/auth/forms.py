5fde82cc169d69229240da7798e16a51d507f26a
henry199101 <331243543@qq.com>
2018-02-19 17:39:14 +0800

    add many

--- flasky/app/auth/forms.py
+++ flasky/app/auth/forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import Required, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
 from ..models import User
 
+
 class LoginForm(FlaskForm):
 	email = StringField('Email', validators=[Required(), Length(1, 64),
 						 Email()])
@@ -31,3 +32,41 @@ class RegistrationForm(FlaskForm):
 	def validate_username(self, field):
 		if User.query.filter_by(username=field.data).first():
 		    raise ValidationError('Usernaem already in use.')
+
+
+class ChangePasswordForm(FlaskForm):
+	old_password = PasswordField('Old password', validators=[Required()])
+	password = PasswordField('New password', validators=[
+	    Required(), EqualTo('password2', message='Passwords must match')])
+	password2 = PasswordField('Confirm new password', validators=[Required()])
+	submit = SubmitField('Update Password')
+
+
+class PasswordResetRequestForm(FlaskForm):
+	email = StringField('Old password', validators=[Required(), Length(1, 64),
+							Email()])
+	submit = SubmitField('Reset Password')
+
+
+class PasswordResetForm(FlaskForm):
+	email = StringField('Email', validators=[Required(), Length(1, 64),
+						 Email()])
+	password = PasswordField('New Password', validators=[
+	    Required(), EqualTo('password2', message='Passwords must be match')])
+	password2 = PasswordField('Confirm password', validators=[Required()])
+	submit = SubmitField('Reset Password')
+
+	def validate_email(self, field):
+		if User.query.filter_by(email=field.data).first() is None:
+			raise ValidationError('Unknown email address.')
+
+
+class ChangeEmailForm(FlaskForm):
+	email = StringField('New Email', validators=[Required(), Length(1, 64),
+						     Email()])
+	password = PasswordField('Password', validators=[Required()])
+	submit = SubmitField('Update Email Address')
+
+	def validate_email(self, field):
+		if User.query.filter_by(email=field.data).first():
+			raise ValidationError('Email already registered.')
